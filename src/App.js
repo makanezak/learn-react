8580a7496db184ee49d047bc8f57f68a4749c884
makanezak <130999456+makanezak@users.noreply.github.com>
2023-04-17 14:40:52 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Azul, Salam Alikoum</h1>
+      <h2>Remdan amarvouh elahna inchAllah</h2>
+    </div>
+  );
+}
